4ff160cc0c66d01857233ccf8db5ec0b34e29a2f
John Bryan <jbryan@mgmresorts.com>
2025-10-15 23:32:20 -0700

    second commit

--- interface/script.js
+++ interface/script.js
@@ -1,6 +1,6 @@
 class GurdjieffBot {
     constructor() {
-        this.apiUrl = 'http://localhost:8000';
+        this.apiUrl = 'http://127.0.0.1:8000';
         this.isConnected = false;
         this.currentSources = [];
         
